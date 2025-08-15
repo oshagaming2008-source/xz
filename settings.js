bbaec9f2f363f32f53ea5579549760118adef3d9
oshagaming2008-source <oshagaming2008@gmail.com>
2025-08-15 21:04:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'knoffi',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '14703105589', //Your number
+  botName: "Oshii",
+  botOwner: 'osha', // 
+  ownerNumber: '94753485781', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
